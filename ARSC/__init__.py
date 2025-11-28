9b3ac17a67b2ca8751ba6bbf729567cbeb2d61d7
stsnsn <satoshi-nishino@g.ecc.u-tokyo.ac.jp>
2025-11-28 13:46:03 +0900

    modified:   setup.py and init.py

--- ARSC/__init__.py
+++ ARSC/__init__.py
@@ -1,5 +1,5 @@
 # ARSC/__init__.py
 from .core import process_faa, compute_ARSC_extended_counts
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Satoshi Nishino"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="arsc",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=["biopython>=1.79"],
     long_description=long_description,
